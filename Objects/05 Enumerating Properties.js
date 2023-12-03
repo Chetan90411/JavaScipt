41d0a9c4e6e682579619f69f035127ef9e6939f2
Chetan Singh <122774503+Chetan90411@users.noreply.github.com>
2023-12-03 11:43:59 +0530

    Create 05 Enumerating Properties.js

---
+++ Objects/05 Enumerating Properties.js
@@ -0,0 +1,21 @@
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = () => {
+        console.log('Draw');
+    }
+}
+
+const circle = new Circle(10)
+// to enumerate all the keys in an object we use for in loop
+for (let key in circle) {
+    if (typeof circle[key] !== 'function')
+    console.log(key, circle[key]);
+}
+
+// Below Object.keys() function returs all the methods and properties key values as an array but we cannot filter the methods from the object 
+const keys = Object.keys(circle);
+console.log(keys);
+
+// To check the existence of a property in an object use 'in' operator
+if ('radius' in circle)
+    console.log('Circle has a radius')
