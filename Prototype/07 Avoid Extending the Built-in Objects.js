20feab82e375286d48a164c90ff6c834c829d96f
Chetan Singh <122774503+Chetan90411@users.noreply.github.com>
2023-12-04 18:33:58 +0530

    Add files via upload

---
+++ Prototype/07 Avoid Extending the Built-in Objects.js
@@ -0,0 +1,12 @@
+
+Array.prototype.shuffle = function () {
+    //...
+}
+
+const array = [];
+array.shuffle();
+
+// don't modify Objects you don't own
+// don't overwrite methods, don't add new properties and methods and don't remove existing properties of a built-in Object
+
+// there might be code which will be dependent on the built in object so modifying them may cause all sorts of issues
